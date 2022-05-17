d47aa56db7d3e2324a59607bcb738fec95aedee8
Sampath2425 <105580016+Sampath2425@users.noreply.github.com>
2022-05-17 18:16:03 +0530

    Add files via upload

---
+++ FindSecondLargeArray.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+import java.util.Arrays;
+
+public class FindSecondLargeArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] data = {3, 2, 11, 4, 6, 7};
+		
+		Arrays.sort(data);
+		System.out.println(data[data.length-2]);
+		
+
+	}
+
+}
